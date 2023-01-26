ad8a5270d88702402e9042774eea049f3feb4ba2
Jake <jacob.wigal@nps.edu>
2023-01-26 08:35:25 -0800

    fix responsivness

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -41,7 +41,7 @@ function Header() {
   return (
     <header className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top && 'bg-light backdrop-blur-sm shadow-lg'}`}>
       <div className="max-w-6xl mx-auto sm:px-6">
-        <div className="flex items-center justify-between h-16 md:h-20 ml-5 mr-5">
+        <div className="flex items-center justify-between h-16 sm:h-20 ml-5 mr-5">
           {/* Site branding */}
           <div className="flex-shrink-0 mr-4">
             {/* Logo */}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    screens: {
+      'xs': '470px',
+      // => @media (min-width: 640px) { ... }
+      'sm': '600px',
+      // => @media (min-width: 640px) { ... }
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+    }
   },
   plugins: [
     ]
